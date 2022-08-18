be47a1e83ecab2cc7998dd2a572267eabccd75ec
Qianli Wu <97045514+Qianli-Wu@users.noreply.github.com>
2022-08-18 11:04:55 +0800

    added Fashionmnist_truncated()

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ from sklearn.metrics import confusion_matrix
 from torchvision import datasets
 
 from model import *
-from datasets import CIFAR10_truncated, CIFAR100_truncated, ImageFolder_custom
+from datasets import CIFAR10_truncated, CIFAR100_truncated, ImageFolder_custom, Fashionmnist_truncated
 
 logging.basicConfig()
 logger = logging.getLogger()
